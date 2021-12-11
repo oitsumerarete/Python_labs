7eb569a82d0894d49c1189bb2514b251ebf7215a
oitsumerarete <71769533+oitsumerarete@users.noreply.github.com>
2021-12-11 23:18:51 +0300

    fixed issue

--- lab_2/ep_2/cargo.py
+++ lab_2/ep_2/cargo.py
@@ -1,33 +1,18 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("flights.csv")
+df = pd.read_csv("flights.csv", index_col=0)
+df.reset_index()
 
-df.drop(df.columns[0], axis=1)
-
-CARGO = list(set(df.CARGO))
-PRICE = []
-WEIGHT = []
-FLIGHTS = []
-
-for company in CARGO:
-    PRICE.append(df.loc[df.CARGO == company].PRICE.sum())
-    WEIGHT.append(df.loc[df.CARGO == company].WEIGHT.sum())
-    FLIGHTS.append(df.loc[df.CARGO == company].shape[0])
-
+newdata = df.groupby('CARGO').sum()
+newdata['COUNTS'] = df.groupby('CARGO').count()['PRICE'].values
 fig, axs = plt.subplots(ncols=3)
-axs[0].set_title('Number of flights')
-axs[1].set_title('Total value of goods transported')
-axs[2].set_title('Total mass transported')
-fig.set_figwidth(10)
-plt.subplots_adjust(wspace=0.5)
 
-for company in CARGO:
-    print(str(company), ": ", len(df.loc[df.CARGO == company].CARGO))
-    number_of_flights = len(df.loc[df.CARGO == company].CARGO)
-axs[0].bar(CARGO, WEIGHT, color='red')
-axs[1].bar(CARGO, PRICE, color='royalblue')
-axs[2].bar(CARGO, FLIGHTS, color='magenta')
+for i in newdata.columns:
+    newdata[i].plot.bar(ax=axs[list(newdata.columns).index(i)],
+                        fontsize=12, figsize=(10, 8), title=i, color=['c', 'm', 'royalblue'])
+    axs[list(newdata.columns).index(i)].title.set_size(15)
+fig.tight_layout(pad=1.0)
 plt.savefig('episode_2.png')
 
 
